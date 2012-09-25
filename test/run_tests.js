8fe71c654125cf7bbb84a11f699602bf9daee687
Christian Boulanger <c.boulanger@qxtransformer.org>
2012-09-25 05:45:25 -0400

    Replaced tabs with 2 spaces for consistency

--- test/run_tests.js
+++ test/run_tests.js
@@ -1,36 +1,39 @@
-
 var testBackend = require('./acl_test').testBackend,
-    async = require('async');
+  async = require('async');
 
 var tests = [
-  
+
   /* Memory */
-  function(cb){
-    console.log("Testing memory backend"); 
+  function(cb) {
+    console.log("Testing memory backend");
     testBackend("memory", {}, cb);
   },
   
   /* Redis */
-  function(cb){
-    console.log("Testing Redis backend"); 
-    var options = { host:'127.0.0.1', port:6379, password: null};
+  function(cb) {
+    console.log("Testing Redis backend");
+    var options = {
+      host: '127.0.0.1',
+      port: 6379,
+      password: null
+    };
     testBackend("redis", options, cb);
   },
   
   /* MongoDB */
-  function(cb){
-    console.log("Testing MongoDB backend"); 
-    var url =  "mongodb://127.0.0.1:27017/acltest";
+  function(cb) {
+    console.log("Testing MongoDB backend");
+    var url = "mongodb://127.0.0.1:27017/acltest";
     testBackend("mongodb", url, cb);
   }
+
 ];
 
 // run tests
-async.forEachSeries(tests,function(test,cb){
-  test(function(results){
+async.forEachSeries(tests, function(test, cb) {
+  test(function(results) {
     var exitCode = results.honored === results.total ? 0 : -1;
     exitCode && process.exit(exitCode);
     cb();
   });
 });
-
