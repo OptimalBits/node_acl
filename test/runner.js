67ec8b383d61deeaf72cac62d7fc39f8d0182a96
Christopher Trudel <christophertrudel@Christophers-MacBook-Pro.local>
2014-08-01 12:43:47 -0600

    moved schema creation to runner.js

--- test/runner.js
+++ test/runner.js
@@ -72,26 +72,49 @@ describe('Memory', function () {
   run()
 })
 
+
 describe('Postgres', function () {
   before(function (done) {
     var self = this
       , knex = require('knex')
-  
-  var db = knex({
-    client: 'postgres',
-    connection: {
-      host: '127.0.0.1',
-      port: 5432,
-      user: 'postgres',
-      database: 'travis_ci_test'
-    }
+   
+    var db = knex({
+      client: 'postgres',
+      connection: {
+        host: '192.168.59.103',
+        port: 5433,
+        user: 'blnapi',
+        password: 'blnapi',
+        database: 'blnapi'
+      }
+    });
+    
+    var downSql = 'DROP TABLE IF EXISTS "acl_meta";'+
+      'DROP TABLE IF EXISTS "acl_resources";'+
+      'DROP TABLE IF EXISTS "acl_parents";'+
+      'DROP TABLE IF EXISTS "acl_users";'+
+      'DROP TABLE IF EXISTS "acl_permissions";'
+    ;
+    var upSql = 'CREATE TABLE acl_meta (key TEXT NOT NULL PRIMARY KEY, value TEXT[][] NOT NULL);'+
+      'CREATE TABLE acl_resources (key TEXT NOT NULL PRIMARY KEY, value TEXT[][] NOT NULL);'+
+      'CREATE TABLE acl_parents (key TEXT NOT NULL PRIMARY KEY, value TEXT[][] NOT NULL);'+
+      'CREATE TABLE acl_users (key TEXT NOT NULL PRIMARY KEY, value TEXT[][] NOT NULL);'+
+      'CREATE TABLE acl_permissions (key TEXT NOT NULL PRIMARY KEY, value JSON NOT NULL);'
+    ;
+    
+    db.raw(downSql+upSql)
+      .then(function(resp) {
+        self.backend = new Acl.postgresBackend(db, "acl_");
+        done();
+      })
+    ;
+    
   });
-  
-  self.backend = new Acl.postgresBackend(db, "acl");
   
   run();
 });
-  
+
+
 function run() {
   Object.keys(tests).forEach(function (test) {
     tests[test]()
