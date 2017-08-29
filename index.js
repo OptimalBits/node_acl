5789cfdfc6ca5301d7db96b3cc185182c1508d6f
yrong <yrong1997@gmail.com>
2017-08-29 16:00:07 +0800

    调研并引入node-acl实现权限管理和控制(6h)

--- index.js
+++ index.js
@@ -5,6 +5,3 @@ module.exports.__defineGetter__('redisBackend', function(){
 module.exports.__defineGetter__('memoryBackend', function(){
   return require('./lib/memory-backend.js');
 });
-module.exports.__defineGetter__('mongodbBackend', function(){
-  return require('./lib/mongodb-backend.js');
-});
